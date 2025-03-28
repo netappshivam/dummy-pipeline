9b41e29259aad40c57ed6efbf6a08a48c6a56c38
netappshivam <shivam.bisht@netapp.com>
2025-03-28 10:50:40 +0530

    header back to v1

--- common.go
+++ common.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func Xyz() string {
-	currentYear, currentMonth := time.Now().Year()%100, int(time.Now().Month())
-	currentYearStr := fmt.Sprintf("%d", currentYear)
-	currentMonthStr := fmt.Sprintf("%02d", currentMonth)
-	currentTagDateName := currentYearStr + currentMonthStr
+	//currentYear, currentMonth := time.Now().Year()%100, int(time.Now().Month())
+
+	//t := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), time.Now().Hour(), time.Now().Minute(), time.Now().Second(), time.Now().Nanosecond(), time.UTC)
+	//
+	//// Calling ISOWeek() method
+	//_, week := t.ISOWeek()
+	//
+	//fmt.Println(week)
+	//
+	//currentYearStr := fmt.Sprintf("%d", currentYear)
+	//currentMonthStr := fmt.Sprintf("%02d", currentMonth)
+
+	//currentTagDateName := currentYearStr + currentMonthStr
+	currentTagDateName := "v1"
 
 	fmt.Println(currentTagDateName)
 	return currentTagDateName
