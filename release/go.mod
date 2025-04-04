8fe95f0a7fca6e155028d18d5738e39fadd965ea
netappshivam <shivam.bisht@netapp.com>
2025-04-04 11:27:36 +0530

    fix

---
+++ release/go.mod
@@ -0,0 +1,3 @@
+module release_create.go
+
+go 1.24.1
---
+++ release/release_create.go
@@ -0,0 +1,72 @@
+package release
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	const sprint = "2510"
+	fmt.Printf("Creating %s Branch\n", sprint)
+
+	if len(os.Args) < 2 {
+		log.Fatal("new_release_branch_name argument is required")
+	}
+	newReleaseBranchName := os.Args[1]
+
+	lastTag := newReleaseBranchName
+
+	if lastTag != "" {
+		fmt.Printf("Latest and greatest tag is: %s\n", lastTag)
+		remoteBranch, err := gitLsRemote(sprint)
+		if err != nil {
+			log.Fatalf("Error checking remote branch: %v", err)
+		}
+		if remoteBranch != "" {
+			fmt.Printf("Remote branch %s already exists, hence skipping branch creation!!!\n", sprint)
+		} else {
+			fmt.Printf("Creating the branch - %s\n", sprint)
+			if err := gitCheckout(sprint, "tags/"+lastTag); err != nil {
+				log.Fatalf("Error creating branch: %v", err)
+			}
+			if err := gitPush(sprint); err != nil {
+				log.Fatalf("Error pushing branch: %v", err)
+			}
+		}
+	} else {
+		fmt.Printf("Since there is no tag created for the sprint - %s, cutting the release branch based out of master branch\n", sprint)
+		fmt.Printf("Creating the branch - %s\n", sprint)
+		if err := gitCheckout(sprint, "master"); err != nil {
+			log.Fatalf("Error creating branch: %v", err)
+		}
+		if err := gitPush(sprint); err != nil {
+			log.Fatalf("Error pushing branch: %v", err)
+		}
+	}
+}
+
+func gitLsRemote(branch string) (string, error) {
+	cmd := exec.Command("git", "ls-remote", "--heads", "origin", branch)
+	output, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(output)), nil
+}
+
+func gitCheckout(branch, ref string) error {
+	cmd := exec.Command("git", "checkout", "-b", branch, ref)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func gitPush(branch string) error {
+	cmd := exec.Command("git", "push", "origin", branch)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
